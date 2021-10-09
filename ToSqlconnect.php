5081fecb676d059a7551951202e3791959675ca2
hithamj <91483172+hithamj@users.noreply.github.com>
2021-10-09 20:29:22 +0530

    Add files via upload

---
+++ ToSqlconnect.php
@@ -0,0 +1,11 @@
+<?php
+
+$username = "id15696174_customer";
+$password="";
+$server ="localhost";
+$db = "id15696174_customerdetails";
+
+
+$con = mysqli_connect($server,$username,$password,$db);
+
+?>
